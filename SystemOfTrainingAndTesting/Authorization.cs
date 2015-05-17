213ce825231daf1276ba4ed37b05062e164af4a3
Dmitry <tyunkov.dima@gmail.com>
2015-05-17 21:06:52 +0600

    Added testing window. Added user level.

--- SystemOfTrainingAndTesting/Authorization.cs
+++ SystemOfTrainingAndTesting/Authorization.cs
@@ -11,18 +11,29 @@ namespace SystemOfTrainingAndTesting
         /// <summary>
         /// Метод для авторизации пользователя в системе
         /// </summary>
-        /// <param name="loginString">Введенный логин пользователя</param>
-        /// <param name="passwordString">Введенный пароль пользователя</param>
-        internal static bool Auth(string loginString, string passwordString, out string userString)
+        /// <param name="loginString">_in_ Имя пользователя</param>
+        /// <param name="passwordString">_in_ Пароль</param>
+        /// <param name="userString">_out_ Пользователь</param>
+        /// <param name="userLevel">_out_ Уровень доступа пользователя</param>
+        /// <returns></returns>
+        internal static bool Auth(string loginString, string passwordString, out string userString, out int userLevel)
         {
             if (loginString.Equals("root") && passwordString.Equals("root"))
             {
                 userString = "User: " + loginString + " (" + passwordString + ")";
+                userLevel = 0;
+                return true;
+            }
+            else if (loginString.Equals("user") && passwordString.Equals("user"))
+            {
+                userString = "User: " + loginString + " (" + passwordString + ")";
+                userLevel = 1;
                 return true;
             }
             else
             {
                 userString = "Неверное имя пользователя или пароль";
+                userLevel = 2;
                 return false;
             }
         }
--- SystemOfTrainingAndTesting/Form1.cs
+++ SystemOfTrainingAndTesting/Form1.cs
@@ -14,6 +14,8 @@ namespace SystemOfTrainingAndTesting
 {
     public partial class Form1 : Form
     {
+        private string userString;
+        private int userAccess;
         public Form1()
         {
             InitializeComponent();
@@ -21,9 +23,7 @@ namespace SystemOfTrainingAndTesting
 
         private void buttonAuthorization_Click(object sender, EventArgs e)
         {
-            string userString;
-            
-            if (Authorization.Auth(this.textBoxLogin.Text, this.textBoxPassword.Text, out userString))
+            if (Authorization.Auth(this.textBoxLogin.Text, this.textBoxPassword.Text, out userString, out userAccess))
             {
                 #region Убираем элементы с формы
                 this.Controls.Clear();
@@ -55,7 +55,6 @@ namespace SystemOfTrainingAndTesting
                 labelTitle.Location = new System.Drawing.Point(this.ClientSize.Width/2 - labelTitle.Width/2,
                     labelUser.Height + labelUser.Location.Y + 13);
                 this.Controls.Add(labelTitle);
-
                 #endregion
                 #region Добавляем кнопку "Обучение"
                 System.Windows.Forms.Button buttonTraining = new System.Windows.Forms.Button();
@@ -81,17 +80,40 @@ namespace SystemOfTrainingAndTesting
                 buttonTesting.Click += new System.EventHandler(this.buttonTesting_Click);
                 this.Controls.Add(buttonTesting);
                 #endregion
-                #region Добавляем кнопку "Статистика"
-                System.Windows.Forms.Button buttonStatistics = new System.Windows.Forms.Button();
-                buttonStatistics.Parent = this;
-                buttonStatistics.Text = "Статистика";
-                buttonStatistics.Size = new System.Drawing.Size(75 * 3, 23 * 3);
-                buttonStatistics.Location = new System.Drawing.Point(this.ClientSize.Width / 2 - buttonTraining.Width / 2,
-                    buttonTesting.Height + buttonTesting.Location.Y + 13);
-                buttonStatistics.Visible = true;
-                buttonStatistics.TabIndex = 2;
-                buttonStatistics.Click += new System.EventHandler(this.buttonStatistics_Click);
-                this.Controls.Add(buttonStatistics);
+                #region Добавление кнопки в зависимости от привелегий пользователя
+                switch (userAccess)
+                {
+                    case 0:
+                        #region Добавляем кнопку "Управление"
+                        System.Windows.Forms.Button buttonControl = new System.Windows.Forms.Button();
+                        buttonControl.Parent = this;
+                        buttonControl.Text = "Управление";
+                        buttonControl.Size = new System.Drawing.Size(75 * 3, 23 * 3);
+                        buttonControl.Location = new System.Drawing.Point(this.ClientSize.Width / 2 - buttonTraining.Width / 2,
+                            buttonTesting.Height + buttonTesting.Location.Y + 13);
+                        buttonControl.Visible = true;
+                        buttonControl.TabIndex = 2;
+                        buttonControl.Click += new System.EventHandler(this.buttonControl_Click);
+                        this.Controls.Add(buttonControl);
+                        #endregion
+                        break;
+                    case 1:
+                        #region Добавляем кнопку "Статистика"
+                        System.Windows.Forms.Button buttonStatistics = new System.Windows.Forms.Button();
+                        buttonStatistics.Parent = this;
+                        buttonStatistics.Text = "Статистика";
+                        buttonStatistics.Size = new System.Drawing.Size(75 * 3, 23 * 3);
+                        buttonStatistics.Location = new System.Drawing.Point(this.ClientSize.Width / 2 - buttonTraining.Width / 2,
+                            buttonTesting.Height + buttonTesting.Location.Y + 13);
+                        buttonStatistics.Visible = true;
+                        buttonStatistics.TabIndex = 2;
+                        buttonStatistics.Click += new System.EventHandler(this.buttonStatistics_Click);
+                        this.Controls.Add(buttonStatistics);
+                        #endregion
+                        break;
+                    default:
+                        throw new Exception("Неверный уровень прав пользователя",null);
+                }
                 #endregion
                 #endregion
             }
@@ -108,12 +130,71 @@ namespace SystemOfTrainingAndTesting
 
         private void buttonTesting_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Click to testing button", "Click message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            #region Убираем элементы с формы
+            this.Controls.Clear();
+            #endregion
+            #region Размещаем нужные элементы на форме
+            #region Добавляем label для отображения информации о пользователе
+            System.Windows.Forms.Label labelUser = new System.Windows.Forms.Label();
+            labelUser.Parent = this;
+            labelUser.Text = userString;
+            labelUser.AutoSize = true;
+            labelUser.Visible = true;
+            labelUser.Location = new System.Drawing.Point(this.ClientSize.Width - (labelUser.Width + 13), 13);
+            this.Controls.Add(labelUser);
+            #endregion
+            #region Добавляем label для отображения заголовка
+            System.Windows.Forms.Label labelTitle = new System.Windows.Forms.Label();
+            labelTitle.Parent = this;
+            labelTitle.Text = "Выбирите необходимый тест";
+            labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F,
+                System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            labelTitle.AutoSize = true;
+            labelTitle.Visible = true;
+            labelTitle.Location = new System.Drawing.Point(this.ClientSize.Width / 2 - labelTitle.Width / 2,
+                labelUser.Height + labelUser.Location.Y + 13);
+            this.Controls.Add(labelTitle);
+            #endregion
+            #region Добавляем listBox с доступными тестами
+            System.Windows.Forms.ListBox listBoxTests = new System.Windows.Forms.ListBox();
+            listBoxTests.Parent = this;
+            listBoxTests.TabIndex = 0;
+            listBoxTests.Size = new System.Drawing.Size(this.ClientSize.Width - 26, this.ClientSize.Height - labelTitle.Location.Y - labelTitle.Height - 13 * 3 - 23);
+            listBoxTests.Visible = true;
+            listBoxTests.Location = new System.Drawing.Point(13, labelTitle.Height + labelTitle.Location.Y + 13);
+            this.Controls.Add(listBoxTests);
+            #endregion
+            #region Добавляем кнопку "Начать"
+            System.Windows.Forms.Button buttonSelect = new System.Windows.Forms.Button();
+            buttonSelect.Parent = this;
+            buttonSelect.Text = "Выбрать";
+            buttonSelect.Size = new System.Drawing.Size(75, 23);
+            buttonSelect.Location = new System.Drawing.Point(this.ClientSize.Width / 2 - buttonSelect.Width / 2,
+                listBoxTests.Height + listBoxTests.Location.Y + 13);
+            buttonSelect.Visible = true;
+            buttonSelect.TabIndex = 1;
+            buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            this.Controls.Add(buttonSelect);
+            #endregion
+            #endregion
+
+            //MessageBox.Show("Click to testing button", "Click message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void buttonStatistics_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Click to statistics button", "Click message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        private void buttonControl_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click to control button", "Click message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click to select button", "Click message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
